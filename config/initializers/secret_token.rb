bb731c98f937e2c0e94ecb7628179bd11f962aee
Laura Ku <laura@coverhound.com>
2013-02-04 19:56:45 -0800

    Setup stuff
    -create new app
    -add mechanize
    -rvrmc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cupid::Application.config.secret_token = 'f7b731ff5eb704426dc29f41a35a2c1cc6e4be873b8f2cac9c627b47f6e1bc9a21bd14d28fc23ec1f8013412a89a53fca0330c7f30f4e6c463a963e3cbef0b4f'
